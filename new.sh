d3d4781d2bf73a0e3c6cc7f30004cf9610d0dca1
tukainpng <dfern3321@gmail.com>
2024-06-25 10:14:55 -0300

    Simpleficação do site
    Agora, eu não faço mais o uso nem do Bootstrap, nem do Jekyll (ou
    qualquer outro SSG).
    
    Os posts são feitos com a ajuda de um shell-script e a lista de posts
    também é atualizada por este mesmo script.

--- new.sh
+++ new.sh
@@ -1,20 +1,62 @@
 #!/usr/bin/env bash
 
-printf "Título do post: "
-read TITLE
-printf "Descrição do post: "
-read DESCRIPTION
+echo "Nome do post: "
+read TMPNAME
+echo "Descrição do post: "
+read DESCR
+echo "Thumbnail: "
+read THUMB
 
-touch _posts/"$(date +%Y-%m-%d)-$TITLE.md"
-cat << EOF > _posts/"$(date +%Y-%m-%d)-$TITLE.md"
----
-layout: post
-created: $(date +%d/%m/%Y)
-title: $TITLE
-thumb: "???"
-tags:
-description: $DESCRIPTION
----
+POSTNAME="$(echo "$TMPNAME" | tr ' ' '-')"
+
+touch posts/$POSTNAME.html
+cat posts/_template.html > posts/$POSTNAME.html
+
+mkdir -p tmp
+
+head --lines=33 blog.html > tmp/top
+tail --lines=+34 blog.html > tmp/bottom
+
+cat << EOF > tmp/middle
+    <article class="card">
+      <a class="post-link" href="/posts/$POSTNAME.html">
+        <div class="thumbnail">
+          <p class="center">
+            $THUMB
+          </p>
+        </div>
+        <hgroup>
+          <h3>$TMPNAME</h3>
+          <p>$DESCR</p>
+        </hgroup>
+      </a>
+    </article>
 EOF
 
-$EDITOR _posts/"$(date +%Y-%m-%d)-$TITLE.md"
+cat << EOF | wl-copy
+      <div class="post-thumbnail">
+        <p class="center">
+          $THUMB
+        </p>
+      </div>
+      <hgroup>
+        <h1>$TMPNAME</h1>
+        <p>$DESCR</p>
+      </hgroup>
+      <hr>
+      <div class="post-content">
+
+        CONTEÚDO
+
+      </div>
+EOF
+
+cp blog.html blog.html.bk
+
+cat tmp/top     >  blog.html
+printf "\n"     >> blog.html
+cat tmp/middle  >> blog.html
+printf "\n"     >> blog.html
+cat tmp/bottom  >> blog.html
+
+$EDITOR posts/$POSTNAME.html
